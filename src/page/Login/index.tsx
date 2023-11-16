4d967bf0d90ca6235d5099e2b8d7966251daa398
leekyoyoung <lky@cstone.co.kr>
2023-11-16 16:44:48 +0900

    feat: 로그인 기능을 구현한다.

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -1,30 +1,144 @@
-import Inner from "@/components/Inner";
+import { ChangeEvent, FormEventHandler, useState } from "react";
 import styled, { css } from "styled-components";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 
-const LoginTab = () => { 
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId:process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId:process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+
+type LoginForm = {
+    email:string;
+    password:string;
+    name:string;
+}
+
+const LoginTab = ()=> { 
+    const [loginForm, setLoginForm] = useState<LoginForm>({
+        email:"",
+        password:"",
+        name:""
+    })
+
+    
+    type HandleClickLoginProps = {
+        email:string;
+        password:string,
+        name: string;
+    }
+
+    const handleClickLogin = ({email, password, name}: HandleClickLoginProps) => {
+    
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            //signed in
+            const user = userCredential.user;
+            alert("로그인성공");
+            const myName = document.querySelector('.myname');
+            if (myName !== null) {
+                myName.innerHTML=`${name}님 환영합니다`
+            } 
+            
+
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            switch (errorCode) {
+                case "auth/invalid-email": 
+                    alert('잘못된 이메일 형식입니다.');
+                    break;
+                case "auth/missing-password" :
+                    alert('비밀번호를 입력해주세요.');
+                    break;
+                case "auth/invalid-login-credentials" :
+                    alert('잘못된 아이디/비밀번호 입니다.');
+                    break;
+                case "auth/too-many-requests" :
+                    alert('잠시후 다시 시도해주세요.');
+                    break;
+                case "auth/network-request-failed":
+                    alert( "네트워크 연결에 실패 하였습니다.");
+                    break;
+                case "auth/internal-error":
+                    alert( "잘못된 요청입니다.");
+                    break;
+                default :
+                    alert(errorMessage) ;
+                    break;
+            }
+
+            // if(errorCode == "auth/invalid-email") {
+            //     alert('이메일을 입력해주세요')
+            // } else {
+            //     alert(errorCode)
+            // }
+        });
+    }
+    
+    const handleChangeInput = (event:ChangeEvent<HTMLInputElement>)=> {
+        setLoginForm((prev)=> ({
+            ...prev,
+            [event.target.name]:event.target.value
+        }))  
+    }
+
+    // const handleSetInputForm = <P extends keyof LoginForm>(key: P, value:LoginForm[P])=> {
+    //     setLoginForm((prev)=> ({
+    //         ...prev,
+    //         [key]:value
+    //     }))  
+    // }
+
+    // handleSetInputForm('age', 12)
+
     return (
         <LoginContetns>
             <h1>로그인</h1>
-            <Inner>
                 <FormWrapper>
                     <InputWrapper>
                         <InputTitle>아이디</InputTitle>
-                        <InputBox name="id" placeholder="아이디를 입력하세요"/>
+                        <InputBox  name="email" placeholder="아이디를 입력하세요" onChange={handleChangeInput} />
                     </InputWrapper>
                     <InputWrapper>
                         <InputTitle>비밀번호</InputTitle>
-                        <InputBox name="password" type="password" placeholder="비밀번호를 입력하세요" />
+                        <InputBox name="password" type="password" placeholder="비밀번호를 입력하세요" autoComplete="off" onChange={handleChangeInput}/>
                     </InputWrapper>
+                    <InputWrapper>
+                        <InputTitle>이름</InputTitle>
+                        <InputBox name="name" type="text" placeholder="비밀번호를 입력하세요" onChange={handleChangeInput}/>
+                    </InputWrapper>
+
                     <ButtonWrapper>
-                        <button>로그인</button>
+                        <button type="submit" onClick={()=> handleClickLogin({
+                            email:loginForm.email,
+                            password:loginForm.password,
+                            name:loginForm.name
+                        })}>로그인</button>
                         <button>회원가입</button>
+                        <p className="myname"></p>
                     </ButtonWrapper>
                 </FormWrapper>
-            </Inner>
         </LoginContetns>
     )
 }
+
+export default LoginTab;
 
 export const LoginContetns = styled.div`
     padding-top: 80px;
@@ -91,5 +205,3 @@ export const ButtonWrapper = styled.div `
         cursor: pointer;
     }
 `
-
-export default LoginTab;
